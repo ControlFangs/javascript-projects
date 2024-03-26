eca35a2d7e552f200b7021cd45ca717302e1fea9
ControlFangs <163154411+ControlFangs@users.noreply.github.com>
2024-03-26 13:14:25 -0500

    Done

--- data-and-variables/exercises/data-and-variables-exercises.js
+++ data-and-variables/exercises/data-and-variables-exercises.js
@@ -1,11 +1,37 @@
+
 // Declare and assign the variables below
-
+let Nameofthespaceshuttle = "Determination"
+let ShuttleSpeed = 17500
+let DistancetoMars = 225000000
+let DistancetotheMoon = 384400
+let Milesperkilometer = 	0.621
+let milesToMars = 139725000
+let hoursToMars = 7984.285714285715
+let daysToMars  = 332.67857142857144
+let milesTomoon = 238712.4
+let hoursTomoon = 13.64070857142857 
+let daysTomoon = 0.5683628571428571
 // Use console.log to print the 'typeof' each variable. Print one item per line.
-
+console.log(typeof Nameofthespaceshuttle)
+console.log(typeof ShuttleSpeed) 
+console.log(typeof DistancetoMars)
+console.log(typeof DistancetotheMoon) 
+console.log(typeof Milesperkilometer) 
+console.log (typeof milesToMars)
+console.log (typeof hoursToMars)
+console.log (typeof daysToMars) 
+console.log (typeof milesTomoon)
+console.log (typeof hoursTomoon)
+console.log (typeof daysTomoon)
 // Calculate a space mission below
-
+console.log("milesToMars = 139725000")
+console.log("hoursToMars = 7984.285714285715") 
+console.log("daysToMars = 332.67857142857144")
 // Print the results of the space mission calculations below
-
+console.log("Determination will take 332.67857142857144 days to reach Mars.")
 // Calculate a trip to the moon below
-
-// Print the results of the trip to the moon below
+console.log("milesTomoon = 238712.4")
+console.log("hourstomoon = 13.64070857142857")
+console.log("daysTomoon = 0.568362857142857" )
+// Print the results of the trip to the moon below 
+"Determination will take 0.568362857142857 days to reach the moon."
