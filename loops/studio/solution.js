0438fc27351fe51246c8c76673726dfc4109e08f
ControlFangs <163154411+ControlFangs@users.noreply.github.com>
2024-04-01 16:06:30 -0500

    y

--- loops/studio/solution.js
+++ loops/studio/solution.js
@@ -34,7 +34,11 @@ let desserts = ['apple', 'banana', 'more kale', 'ice cream', 'chocolate', 'kiwi'
 
 function askForNumber() {
   numMeals = input.question("How many meals would you like to make?");
+  while(numMeals >6 || numMeals<1 || isNaN(numMeals)){
+  numMeals=input.question("enter a number between 1 and 6")
   
+  return numMeals
+  }
   /// CODE YOUR SOLUTION TO PART B here ///
 
   return numMeals;
@@ -56,16 +60,16 @@ function runProgram() {
   /// Change the final input variable (aka numMeals) here to ensure your solution makes the right number of meals ///
   /// We've started with the number 2 for now. Does your solution still work if you change this value? ///
   
-  let meals = mealAssembly(protein, grains, veggies, beverages, desserts, 2);
-  console.log(meals)
+  // let meals = mealAssembly(protein, grains, veggies, beverages, desserts, 2);
+  // console.log(meals)
   
 
   /// TEST PART B HERE ///
   /// UNCOMMENT the next two lines to test your ``askForNumber`` solution ///
   /// Tip - don't test this part until you're happy with your solution to part A #2 ///
   
-  // let mealsForX = mealAssembly(protein, grains, veggies, beverages, desserts, askForNumber());
-  // console.log(mealsForX);
+  let mealsForX = mealAssembly(protein, grains, veggies, beverages, desserts, askForNumber());
+  console.log(mealsForX);
 
     /// TEST PART C HERE ///
   /// UNCOMMENT the remaining commented lines and change the password1 and password2 strings to ensure your code is doing its job ///
